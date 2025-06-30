95e3cd104a3002b6739432045d542483e36ef601
Sweta <swetajaiswal0910@gmail.com>
2025-06-30 12:56:16 +0100

    feat: setup API integration using axios and environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
 import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+import { getPlacesData } from "./api";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, []);
   return (
     <main className="w-full">
       <Header />
